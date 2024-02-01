d88a8319b6ede682eb520a71ff0bbbe90d5ebd97
Jambr0li <jasonkovalenko@gmail.com>
2024-02-01 07:25:28 -0800

    Fetch data dynamically from postgres database on Vercel

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -14,18 +14,22 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  let totalPaidInvoices = 0;
+  let totalPendingInvoices = 0;
+  let numberOfInvoices = 0;
+  let numberOfCustomers = 0;
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
